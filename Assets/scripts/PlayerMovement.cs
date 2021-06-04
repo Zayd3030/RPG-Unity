faf3fc39718d54e7798502d58755821e39197aab
Zayd Hussain <76216005+Zayd3030@users.noreply.github.com>
2021-06-04 23:24:07 +0100

    Initial commit

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+  
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk; 
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+
+            StartCoroutine(AttackCo());
+
+        }
+
+
+       else if (currentState == PlayerState.walk)
+        {
+
+            UpdateAnimationAndMove();
+
+        }
+       
+
+
+    }
+
+    private IEnumerator AttackCo()
+    {
+
+        animator.SetBool("Attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+
+        {
+            moveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+    }
+    void moveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+            );
+    }
+}
+
+
+
+
